76b002b1996d4dc2abea7f4710e36e57e98f9614
Dimitri Acosta <dimitri316@gmail.com>
2018-03-15 18:04:23 -0700

    Login users with username instead of email

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Curso de Docker
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+                    <a href="https://styde.net/curso-de-docker">https://styde.net/curso-de-docker</a>
                 </div>
             </div>
         </div>
