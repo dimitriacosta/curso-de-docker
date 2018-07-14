fc79496860b093a1c0597c40e25992d14519c723
Dimitri Acosta <dimitri316@gmail.com>
2018-07-14 00:26:46 -0600

    Modify Jenkinsfile to run authentication tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,17 +4,17 @@ node('master') {
     stage('build') {
         git url: 'git@github.com:dimitriacosta/curso-de-docker.git'
 
-        sh "./develop up -d"
+        sh "./develop start"
 
         sh "./develop composer install"
 
         sh 'cp .env.example .env'
         sh './develop art key:generate'
-        sh 'sed -i "s/REDIS_HOST=.*/REDIS_HOST=redis/" .env'
-        sh 'sed -i "s/CACHE_DRIVER=.*/CACHE_DRIVER=redis/" .env'
-        sh 'sed -i "s/SESSION_DRIVER=.*/SESSION_DRIVER=redis/" .env'
     }
     stage('test') {
         sh "APP_ENV=testing ./develop test"
+    }
+    stage('cleanup') {
+        sh "./develop stop"
     }
 }
---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_log_in()
+    {
+        factory(User::class)->create([
+            'username' => 'johndoe',
+        ]);
+        $this->assertGuest();
+
+        $response = $this->get('/');
+        $response->assertRedirect('/login');
+
+        $response = $this->post('/login', [
+            'username' => 'johndoe',
+            'password' => 'secret',
+        ]);
+        $response->assertRedirect('/');
+        $this->assertAuthenticated();
+    }
+
+    /** @test */
+    public function users_cannot_authenticate_with_invalid_credentials()
+    {
+        factory(User::class)->create([
+            'username' => 'johndoe',
+            'password' => bcrypt('secret'),
+        ]);
+        $this->assertGuest();
+
+        $this->from('/login');
+        $response = $this->post('/login', [
+            'username' => 'johndoe',
+            'password' => 'wrongpassword',
+        ]);
+        $response->assertRedirect('/login')
+            ->assertSessionHasErrorsIn('default', ['username']);
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function users_can_register()
+    {
+        $this->from('/register');
+
+        $response = $this->post('/register', [
+            'name' => 'John Doe',
+            'username' => 'johndoe',
+            'email' => 'john@example.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+        $response->assertRedirect('/home');
+        $this->assertDatabaseHas('users', [
+            'name' => 'John Doe',
+            'username' => 'johndoe',
+            'email' => 'john@example.com',
+        ]);
+    }
+
+    /** @test */
+    public function name_is_required_to_register_new_users()
+    {
+        $this->from('/register');
+
+        $response = $this->post('/register', [
+            'username' => 'johndoe',
+            'email' => 'john@example.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+        $response->assertRedirect('/register')
+            ->assertSessionHasErrorsIn('default', ['name']);
+        $this->assertDatabaseMissing('users', [
+            'name' => 'John Doe',
+        ]);
+    }
+
+    /** @test */
+    public function username_is_required_to_register_new_users()
+    {
+        $this->from('/register');
+
+        $response = $this->post('/register', [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+        $response->assertRedirect('/register')
+            ->assertSessionHasErrorsIn('default', ['username']);
+        $this->assertDatabaseMissing('users', [
+            'username' => 'johndoe',
+        ]);
+    }
+
+    /** @test */
+    public function email_is_required_to_register_new_users()
+    {
+        $this->from('/register');
+
+        $response = $this->post('/register', [
+            'name' => 'John Doe',
+            'username' => 'johndoe',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+        $response->assertRedirect('/register')
+            ->assertSessionHasErrorsIn('default', ['email']);
+        $this->assertDatabaseMissing('users', [
+            'email' => 'john@example.com',
+        ]);
+    }
+
+    /** @test */
+    public function password_is_required_to_register_new_users()
+    {
+        $this->from('/register');
+
+        $response = $this->post('/register', [
+            'name' => 'John Doe',
+            'username' => 'johndoe',
+            'email' => 'john@example.com',
+        ]);
+        $response->assertRedirect('/register')
+            ->assertSessionHasErrorsIn('default', ['password']);
+        $this->assertDatabaseMissing('users', [
+            'email' => 'john@example.com',
+        ]);
+    }
+
+    /** @test */
+    public function password_must_be_confirmed_to_register_new_users()
+    {
+        $this->from('/register');
+
+        $response = $this->post('/register', [
+            'name' => 'John Doe',
+            'username' => 'johndoe',
+            'email' => 'john@example.com',
+            'password' => 'secret',
+        ]);
+        $response->assertRedirect('/register')
+            ->assertSessionHasErrorsIn('default', ['password']);
+        $this->assertDatabaseMissing('users', [
+            'email' => 'john@example.com',
+        ]);
+    }
+
+    /** @test */
+    public function show_env_variable()
+    {
+        $this->assertEquals('STYDE12345.#!', env('TEST_KEY'));
+    }
+}
